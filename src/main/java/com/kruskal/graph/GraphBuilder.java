0d8e687a0357b258ec1bf4a6c9b468855561072f
Allonario <markush2004@mail.ru>
2023-07-06 16:01:14 +0300

    Added file reader/writer and graph builder

---
+++ src/main/java/com/kruskal/graph/GraphBuilder.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+public class GraphBuilder {
+
+    private Graph graph;
+    public GraphBuilder(){
+            graph = new Graph();
+    };
+
+    public boolean deleteNode(int nodeId){
+        return graph.deleteNode(nodeId);
+    }
+
+    public boolean deleteEdge(int edgeId){
+        return graph.deleteEdge(edgeId);
+    }
+
+    public boolean addNode(int newNodeId){
+        return graph.addNode(newNodeId);
+    }
+    public boolean addEdge(int firstNodeId, int secondNodeId, int weight, int edgeId){
+        return graph.addEdge(firstNodeId, secondNodeId, weight, edgeId);
+    }
+
+    public boolean isValid(){
+        return graph.isValid();
+    }
+
+    public void printGraph(){
+        graph.printGraph();
+    }
+
+    public List<EdgeData> getEdgesData(){
+        return graph.getEdgesData();
+    }
+
+    public void resetGraph(){
+        graph = new Graph();
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
+}
